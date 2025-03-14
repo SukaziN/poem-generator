2fc4abede5c6c1b7a35d99af3918c69a3c9f2ca8
SukaziN <nkosiyazisukazie@gmail.com>
2025-03-14 11:40:49 +0000

    added js functionality and modified css

--- poem generator/script.js
+++ poem generator/script.js
@@ -1,1 +1,15 @@
-alert("Roses are red, violets are blue...");
+document.addEventListener("DOMContentLoaded", function () {
+  function generatePoem(event) {
+    event.preventDefault();
+    let poem = document.querySelector("#poem");
+    poem.innerHTML = "Say youll remember me standing in a nice dress";
+  }
+
+  let formElement = document.querySelector("#submit-form");
+  formElement.addEventListener("submit", generatePoem);
+});
+new Typewriter("#typewriter", {
+  strings: ["Say you'll remember me standing in a nice dress"],
+  autoStart: true,
+  delay: 1,
+});
