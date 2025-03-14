ffa1e6dc54b975aebfd33f679926ea02f20119df
SukaziN <nkosiyazisukazie@gmail.com>
2025-03-14 16:41:27 +0000

    added axios and made AI functional in JavaScript

--- poem generator/script.js
+++ poem generator/script.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
-  function generatePoem(event) {
+  let formElement = document.querySelector("#submit-form");
+  formElement.addEventListener("submit", function (event) {
     event.preventDefault();
-    new Typewriter("#poem", {
-      strings: ["Say you'll remember me standing in a nice dress"],
-      autoStart: true,
-      delay: 75,
-      loop: false,
-      cursor: "",
+    generatePoem();
+  });
+  function generatePoem() {
+    let apiKey = "6f68db0a5tf8734ofa9df0238e83067c";
+    let prompt = "Generate a short 4 line poem about nature in French";
+    let context = "You are a Shakespear level poet with a love for nature";
+    let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(function (response) {
+      new Typewriter("#poem", {
+        strings: [response.data.answer],
+        autoStart: true,
+        delay: 75,
+        loop: false,
+        cursor: "",
+      });
     });
   }
-
-  let formElement = document.querySelector("#submit-form");
-  formElement.addEventListener("submit", generatePoem);
 });
-
-let apiKey = "6f68db0a5tf8734ofa9df0238e83067c";
-let prompt = "Generate a short 4 line poem about nature in French";
-let context = "You are a Shakespear level poet with a love for nature";
-let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
