7dfe2c5f144a5bd65cf7636ebf390b41ea37ae2a
SukaziN <nkosiyazisukazie@gmail.com>
2025-03-14 13:34:18 +0000

    made typewriter functional and began AI code

--- poem generator/script.js
+++ poem generator/script.js
@@ -1,15 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
   function generatePoem(event) {
     event.preventDefault();
-    let poem = document.querySelector("#poem");
-    poem.innerHTML = "Say youll remember me standing in a nice dress";
+    new Typewriter("#poem", {
+      strings: ["Say you'll remember me standing in a nice dress"],
+      autoStart: true,
+      delay: 75,
+      loop: false,
+      cursor: "",
+    });
   }
 
   let formElement = document.querySelector("#submit-form");
   formElement.addEventListener("submit", generatePoem);
 });
-new Typewriter("#typewriter", {
-  strings: ["Say you'll remember me standing in a nice dress"],
-  autoStart: true,
-  delay: 1,
-});
+
+let apiKey = "6f68db0a5tf8734ofa9df0238e83067c";
+let prompt = "Generate a short 4 line poem about nature in French";
+let context = "You are a Shakespear level poet with a love for nature";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
